b9b6fee51adcdf153064272927f61e91fed73f0b
Daniel Moczarski <d.moczarski@gmx.de>
2024-10-11 18:06:16 +0200

    Added simple class reflection system to obtain class inheritence hierarchy during runtime.

--- Ganymede/src/Ganymede/Core/Core.h
+++ Ganymede/src/Ganymede/Core/Core.h
@@ -5,30 +5,33 @@
 #include <unordered_map>
 #include <typeindex>
 
-typedef size_t ClassID;
+namespace Ganymede
+{
+    typedef size_t ClassID;
+    static constexpr const ClassID g_InvalidClassID = -1;
 
 #define GM_GENERATE_CLASS_ID(VarName, Class) static const ClassID id = static_cast<ClassID>(std::type_index(typeid(Class)).hash_code());
 #define GM_GENERATE_STATIC_CLASS_ID(Class) static ClassID GetStaticClassID() { GM_GENERATE_CLASS_ID(id, Class); return id; }
 
-// Make sure to implement all platform interfaces in the "Platform" folder for supported platforms
+    // Make sure to implement all platform interfaces in the "Platform" folder for supported platforms
 #ifdef GM_PLATFORM_WINDOWS
-	#ifdef GM_BUILD_DLL
-		#define GANYMEDE_API __declspec(dllexport)
-	#else
-		#define GANYMEDE_API __declspec(dllimport)
-	#endif // GM_BUILD_DLL
+#ifdef GM_BUILD_DLL
+#define GANYMEDE_API __declspec(dllexport)
 #else
-	#error Ganymede only supported on Windows!
+#define GANYMEDE_API __declspec(dllimport)
+#endif // GM_BUILD_DLL
+#else
+#error Ganymede only supported on Windows!
 #endif // GM_PLATFORM_WINDOWS
 
 #define BIT(x) (1 << x)
 
 #ifndef GM_RETAIL
-	#define GM_CORE_ASSERTS_ENABLED
+#define GM_CORE_ASSERTS_ENABLED
 #endif // GM_RETAIL
 
 #ifdef GM_CORE_ASSERTS_ENABLED
-	#define GM_CORE_ASSERT(condition, message)									\
+#define GM_CORE_ASSERT(condition, message)									\
 		{																		\
 			if (!(condition))													\
 			{																	\
@@ -40,5 +43,40 @@ typedef size_t ClassID;
 			}																	\
 		}
 #else
-	#define GM_CORE_ASSERT(condition, message)
+#define GM_CORE_ASSERT(condition, message)
 #endif
+
+// This is a tiny reflection system to obtain class inheritance hierarchy during runtime. Use when required. Not all classes need to carry this information.
+#define GM_GENERATE_CLASSTYPEINFO(classname, parentClassName) inline static ClassTypeInfoImpl<classname, parentClassName> m_ClassInfoType;
+    struct ClassTypeInfo
+    {
+        ClassTypeInfo() = default;
+        ~ClassTypeInfo() = default;
+
+        virtual const ClassTypeInfo* GetParentClassInfoType() const
+        {
+            return nullptr;
+        }
+
+        virtual ClassID GetClassID() const { return g_InvalidClassID; }
+
+        static ClassTypeInfo s_ClassInfoType;
+    };
+
+    template <class Clazz, class ParentClazz>
+    struct ClassTypeInfoImpl : public ClassTypeInfo
+    {
+        const ClassTypeInfo* GetParentClassInfoType() const override
+        {
+            // Some minor compile time checks. Still needs to ensure classes implement the neede template correctly!
+            static_assert(!std::is_same<Clazz, ParentClazz>::value && (std::is_same<ParentClazz, ClassTypeInfo>::value || std::is_base_of<ParentClazz, Clazz>::value), "Clazz must be direct subclass of ParentClazz");
+            return &ParentClazz::s_ClassInfoType;
+        }
+
+        ClassID GetClassID() const override
+        {
+            GM_GENERATE_CLASS_ID(id, Clazz);
+            return id;
+        }
+    };
+}
